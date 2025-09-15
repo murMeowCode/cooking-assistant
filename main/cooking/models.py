b75c34ff95d000a9588129f3fb1ef49eac98393a
johnthesilver <johnthesilver22@gmail.com>
2025-09-15 16:39:09 +0300

    create models

--- main/cooking/models.py
+++ main/cooking/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Dish(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    instructions = models.TextField()
+
+    def __str__(self):
+        return self.title 
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
+    
+class DishIngredient(models.Model):
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.ingredient.name} for {self.dish.title}"
