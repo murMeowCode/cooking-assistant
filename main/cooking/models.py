1ae34e7a9b4441edd587d606e98b8c3df8a56319
johnthesilver <johnthesilver22@gmail.com>
2025-09-15 17:08:50 +0300

    create api

--- main/cooking/models.py
+++ main/cooking/models.py
@@ -4,6 +4,9 @@ class Dish(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     instructions = models.TextField()
+    cooktime = models.IntegerField(help_text="Cook time in minutes", null=True, blank=True)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True)
+    starred = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title 
@@ -21,3 +24,9 @@ class DishIngredient(models.Model):
 
     def __str__(self):
         return f"{self.quantity} of {self.ingredient.name} for {self.dish.title}"
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- main/cooking/views.py
+++ main/cooking/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework.mixins import ListModelMixin, UpdateModelMixin
+from rest_framework.generics import ListAPIView
+from main.cooking.models import Dish
+from main.cooking.serializers import DishSerializer
 
-# Create your views here.
+class DishViewSet(ListModelMixin, UpdateModelMixin):
+    starred_dishes = Dish.objects.filter(starred=True)
+    serializer = DishSerializer(starred_dishes, many=True)
+    queryset = starred_dishes
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)  
+    
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    
+class AllDishListView(ListAPIView):
+    queryset = Dish.objects.all()
+    serializer_class = DishSerializer
+    filterset_fields = ['category__name']
+    search_fields = ['title', 'description', 'instructions', 'ingredients__ingredient__name']
+    ordering_fields = ['title', 'cooktime']
+    ordering = ['title']
+    pagination_class = None  # Disable pagination to return all results
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category = self.request.query_params.get('category')
+        if category:
+            queryset = queryset.filter(category__name=category)
+        return queryset
+    
+class PossibleDishesListView(ListAPIView):
+    serializer_class = DishSerializer
+    pagination_class = None  # Disable pagination to return all results
+
+    def get_queryset(self):
+        ingredients = self.request('ingredients')
+        if not ingredients:
+            return Dish.objects.none()
+        
+        dishes = Dish.objects.all()
+        possible_dishes = []
+        for dish in dishes:
+            dish_ingredients = dish.dishingredient_set.all()
+            if all(any(ing.name == ingredient for ing in dish_ingredients.values_list('ingredient__name', flat=True)) for ingredient in ingredients):
+                possible_dishes.append(dish)
+        
+        return possible_dishes
