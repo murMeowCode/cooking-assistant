740568f0c6afe6203d10194ef478f01154059c55
murMeowCode <grisha23.01.2004@gmail.com>
2025-09-23 19:11:08 +0300

    refactor dish lists

--- BACKEND/main/cooking/views.py
+++ BACKEND/main/cooking/views.py
@@ -35,6 +35,15 @@ class AllDishListView(ListAPIView):
     queryset = Dish.objects.all()
     serializer_class = DishSerializer
     renderer_classes = [JSONRenderer]
+    pagination_class = CustomPagination  # Добавляем пагинацию
+    ordering_fields = ['title', 'cooktime']  # Оставляем сортировку для клиента
+    ordering = ['title']
+
+    # Убираем всю фильтрацию, оставляем только базовый queryset
+
+
+class PossibleDishesListView(APIView):
+    pagination_class = CustomPagination
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     
     # Фильтрация по точным значениям
@@ -47,41 +56,6 @@ class AllDishListView(ListAPIView):
     ordering_fields = ['title', 'cooktime']
     ordering = ['title']
 
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        
-        # Фильтрация по категории (точное совпадение)
-        category = self.request.query_params.get('category')
-        if category:
-            queryset = queryset.filter(category__name=category)
-        
-        # Фильтрация по типу (точное совпадение)
-        dish_type = self.request.query_params.get('type')
-        if dish_type:
-            queryset = queryset.filter(type__name=dish_type)
-        
-        # Фильтрация по диапазону cooktime
-        cooktime_min = self.request.query_params.get('cooktime_min')
-        cooktime_max = self.request.query_params.get('cooktime_max')
-        
-        if cooktime_min and cooktime_max:
-            queryset = queryset.filter(cooktime__gte=cooktime_min, cooktime__lte=cooktime_max)
-        elif cooktime_min:
-            queryset = queryset.filter(cooktime__gte=cooktime_min)
-        elif cooktime_max:
-            queryset = queryset.filter(cooktime__lte=cooktime_max)
-        
-        # Фильтрация по частичному совпадению в title (альтернативный вариант)
-        title_search = self.request.query_params.get('title')
-        if title_search:
-            queryset = queryset.filter(title__icontains=title_search)
-        
-        return queryset
-    
-
-class PossibleDishesListView(APIView):
-    pagination_class = CustomPagination
-    
     def post(self, request):
         user_ingredients = request.data.get('ingredients', [])
         willing_to_buy = request.data.get('willing_to_buy', False)
@@ -108,6 +82,9 @@ class PossibleDishesListView(APIView):
             dishes = dishes.filter(matching_ingredients_count=F('total_ingredients_count'))
             dishes = dishes.order_by('title')
         
+        # Применяем фильтрацию из query parameters
+        dishes = self.apply_filters(dishes, request)
+        
         # Пагинация
         paginator = self.pagination_class()
         page = paginator.paginate_queryset(dishes, request, view=self)
@@ -119,6 +96,41 @@ class PossibleDishesListView(APIView):
         )
         
         return paginator.get_paginated_response(serializer.data)
+    
+    def apply_filters(self, queryset, request):
+        """Применяет фильтрацию к queryset"""
+        
+        # Фильтрация по категории (точное совпадение)
+        category = request.query_params.get('category')
+        if category:
+            queryset = queryset.filter(category__name=category)
+        
+        # Фильтрация по типу (точное совпадение)
+        dish_type = request.query_params.get('type')
+        if dish_type:
+            queryset = queryset.filter(type__name=dish_type)
+        
+        # Фильтрация по диапазону cooktime
+        cooktime_min = request.query_params.get('cooktime_min')
+        cooktime_max = request.query_params.get('cooktime_max')
+        
+        if cooktime_min and cooktime_max:
+            queryset = queryset.filter(cooktime__gte=cooktime_min, cooktime__lte=cooktime_max)
+        elif cooktime_min:
+            queryset = queryset.filter(cooktime__gte=cooktime_min)
+        elif cooktime_max:
+            queryset = queryset.filter(cooktime__lte=cooktime_max)
+        
+        # Фильтрация по частичному совпадению в title
+        title_search = request.query_params.get('title')
+        if title_search:
+            queryset = queryset.filter(title__icontains=title_search)
+        
+        # Применяем стандартные фильтры DRF
+        for backend in self.filter_backends:
+            queryset = backend().filter_queryset(request, queryset, self)
+        
+        return queryset
     
     def get_paginated_response(self, data):
         return Response({
